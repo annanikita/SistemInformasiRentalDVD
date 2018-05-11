7600da37414f9f5d89e1e468a9e379f0fa5f4a95
Annastasya Purnama Nikita <31577022+annanikita@users.noreply.github.com>
2018-05-12 03:21:04 +0700

    Update checkLogin.php

--- mainApp/checkLogin.php
+++ mainApp/checkLogin.php
@@ -8,4 +8,12 @@ include('./config/db/mysql/db.conf');
 //get login variabel
 $username = $this -> input -> post['txtUsername'];
 $password = $this -> input -> post['txtPassword'];
+
+//change password to md5
+$password = md5($password);
+
+//kueri to search username & password
+$kCheckUser = $con -> query("SELECT id FROM tbl_user WHERE username='$username' AND password='$password';");
+$totalUser = mysqli_num_rows($kCheckUser);
+
 ?>
