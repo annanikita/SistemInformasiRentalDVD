e341fc71345e0fda3c1c6399b59f28c7c23d01a7
firdanadia <39202426+firdanadia@users.noreply.github.com>
2018-05-19 22:16:10 +0700

    Update vueApiLogin.js

--- apiBank/vueApiLogin.js
+++ apiBank/vueApiLogin.js
@@ -9,6 +9,8 @@ import { requireAuth } from '../../utils/auth';
 Vue.use(Router);
 
 export default new Router({
+  const dataLogin as arrayDataLogin as arrLog['email'] = vue_component('input':'txtEmail'->value());
+  const arrLog['password'] = vue_component('input':'txtPassword'->value());
   routes: [
     {
       path: '/',
@@ -16,6 +18,7 @@ export default new Router({
       component: loginDiv,
     },
     {
+       requireAuth = new vue(Auth::);
       path: '/apiLogin/systemLogin/vueLogin',
       name: 'loginDiv',
       beforeEnter: requireAuth,
